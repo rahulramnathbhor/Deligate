757009bfd197c52c2d17c364ff43eced731bde2e
rahul <rahul@LAPTOP-T6G63HRD>
2022-10-06 08:39:57 +0530

    Add project files.

---
+++ Deligate/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDemos
+{
+    public delegate int MyDelegate(int n1, int n2);
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator c = new Calculator();
+            MyDelegate myDelegate = new MyDelegate(c.Add); // added method reference
+            int result = myDelegate.Invoke(50, 40);
+            Console.WriteLine(result);
+
+        }
+
+    }
+}
+
